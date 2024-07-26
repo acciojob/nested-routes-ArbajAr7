71c3ee1cdb596a44068784ffe17ce56ca185eb7e
ArbajAr7 <arbajrmacr786@gmail.com>
2024-07-26 20:46:29 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 
 import React from "react";
-import './../styles/App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+
+import Home from "./Home";
+import Women from "./Women";
+import Grooming from "./Grooming";
+import Shirt from "./Shirt";
+import Trouser from "./Trouser";
+import Jewellery from "./Jewellery";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <h1>React Router</h1>
 
-export default App
+      <ul>
+      <li> <NavLink to="/">Home</NavLink></li> 
+      <li> <NavLink to="/women">Women</NavLink></li> 
+      </ul>
+
+      <Routes>
+      
+        <Route path="/" element={<Home />} />
+
+        <Route path="/women" element={<Women />}>
+          <Route path="Grooming" element={<Grooming />} />
+          <Route path="Shirt" element={<Shirt />} />
+          <Route path="Trouser" element={<Trouser />} />
+          <Route path="Jewellery" element={<Jewellery />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
